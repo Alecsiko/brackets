46b55931aa9b97f3b5e88060615ed5c348e72796
Aleksey <104904174+Alecsiko@users.noreply.github.com>
2022-08-10 18:49:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
+  const newArr = bracketsConfig.map(currrentValue => currrentValue.join(''))
+  console.log(newArr);
+  for(i=0;i<newArr.length;i++){
+    if(str.includes(newArr[i])) {
+      str = str.replace(newArr[i], '') // находим в массиве эл-т str,меняем его на ''
+      console.log(str);
+      i=-1   // cоотвественно,длинна массива уменьшилась на '' т.е на 1 элент совпадения
+    }
+    
+  }
+  
+
+  return !str  
   // your solution
 }
